3d19bbba3f112bd423168a66f8371b60f592a696
Artem Kiselev <kuzuru.dev@gmail.com>
2023-01-08 21:50:46 +0500

    feat: Implemented GET snippet URL

--- internal/entity/snippet/db/postgresql.go
+++ internal/entity/snippet/db/postgresql.go
@@ -2,7 +2,8 @@ package db
 
 import (
 	"context"
-
+	"errors"
+	"github.com/jackc/pgx/v4"
 	"github.com/kuzuru/codesync/internal/entity/snippet"
 	"github.com/kuzuru/codesync/pkg/db/postgresql"
 )
@@ -23,8 +24,25 @@ func (r repository) Create(ctx context.Context, snippet *snippet.Snippet) error 
 }
 
 func (r repository) GetByLink(ctx context.Context, link string) (*snippet.Snippet, error) {
-	//TODO implement me
-	panic("implement me")
+	query := `SELECT
+    	id, link, title, lang, code, author_id, is_anonymous, created_at, updated_at
+		FROM public.snippets
+		WHERE link = $1;`
+
+	var s snippet.Snippet
+
+	err := r.client.QueryRow(ctx, query, link).Scan(&s.ID, &s.Link, &s.Title, &s.Language, &s.Code, &s.AuthorID, &s.IsAnonymous, &s.CreatedAt, &s.UpdatedAt)
+
+	// Check if no rows was found with ErrNoRows
+	if err != nil {
+		if errors.Is(err, pgx.ErrNoRows) {
+			return nil, errors.New("nothing was found :(")
+		}
+
+		return nil, err
+	}
+
+	return &s, nil
 }
 
 func (r repository) UpdateSnippet(ctx context.Context, snippet snippet.Snippet) error {
--- internal/entity/snippet/snippet.go
+++ internal/entity/snippet/snippet.go
@@ -1,13 +1,15 @@
 package snippet
 
+import "github.com/jackc/pgtype"
+
 type Snippet struct {
-	ID          int    `json:"id"`
-	Link        string `json:"link"`
-	Title       string `json:"title"`
-	Language    string `json:"language"`
-	Code        string `json:"code"`
-	AuthorID    int    `json:"author_id"`
-	IsAnonymous bool   `json:"is_anonymous"`
-	CreatedAt   string `json:"created_at"`
-	UpdatedAt   string `json:"updated_at"`
+	ID          int                `json:"id"`
+	Link        string             `json:"link"`
+	Title       string             `json:"title"`
+	Language    string             `json:"language"`
+	Code        string             `json:"code"`
+	AuthorID    int                `json:"author_id"`
+	IsAnonymous bool               `json:"is_anonymous"`
+	CreatedAt   pgtype.Timestamptz `json:"created_at"`
+	UpdatedAt   pgtype.Timestamptz `json:"updated_at"`
 }
