8207686ddccad5718d5e13236d4af39eeb01ad3d
Artem Kiselev <kuzuru.dev@gmail.com>
2023-01-08 23:27:06 +0500

    feat: Implemented GetByID in snippet Storage

--- internal/entity/snippet/storage.go
+++ internal/entity/snippet/storage.go
@@ -5,6 +5,7 @@ import "context"
 type Storage interface {
 	Create(ctx context.Context, snippet *Snippet) error
 	GetByLink(ctx context.Context, link string) (*Snippet, error)
+	GetByID(ctx context.Context, id int) (*Snippet, error)
 	UpdateSnippet(ctx context.Context, snippet Snippet) error
 	DeleteByID(ctx context.Context, id string) error
 }
